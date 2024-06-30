da0afcb19626f0ed89c4d4d2d24712f1de124999
Cosimo <46655466+encoded@users.noreply.github.com>
2024-06-30 16:09:12 +0100

    Logging devices info in ex_portaudio

--- cpp/examples/ex_portaudio.cpp
+++ cpp/examples/ex_portaudio.cpp
@@ -19,6 +19,34 @@ int main(int argc, char* argv[])
         fprintf(stderr, "PortAudio error: %s\n", Pa_GetErrorText(err));
         return EXIT_FAILURE;
     }
+    printf("Portaudio initialized.\n");
+
+    // Listing available devices and printing info
+    int numDevices = Pa_GetDeviceCount();
+    if( numDevices < 0 )
+    {
+        fprintf(stderr, "PortAudio error: %s\n", Pa_GetErrorText(numDevices));
+        return EXIT_FAILURE;
+    }
+
+    printf("Getting %i device(s) info...\n", numDevices);
+
+    const PaDeviceInfo *deviceInfo;
+    for (int i = 0; i < numDevices; i++) {
+        deviceInfo = Pa_GetDeviceInfo(i);
+
+        printf("Device %d:\n", i + 1);
+        printf("  Name: %s\n", deviceInfo->name);
+        printf("  Host API Index: %d\n", deviceInfo->hostApi);
+        printf("  Max Input Channels: %d\n", deviceInfo->maxInputChannels);
+        printf("  Max Output Channels: %d\n", deviceInfo->maxOutputChannels);
+        printf("  Default Low Input Latency: %.3f seconds\n", deviceInfo->defaultLowInputLatency);
+        printf("  Default Low Output Latency: %.3f seconds\n", deviceInfo->defaultLowOutputLatency);
+        printf("  Default High Input Latency: %.3f seconds\n", deviceInfo->defaultHighInputLatency);
+        printf("  Default High Output Latency: %.3f seconds\n", deviceInfo->defaultHighOutputLatency);
+        printf("  Default Sample Rate: %.2f Hz\n", deviceInfo->defaultSampleRate);
+        printf("\n");
+    }
 
     err = Pa_Terminate();
     if(err != paNoError)
@@ -26,6 +54,7 @@ int main(int argc, char* argv[])
         fprintf(stderr, "PortAudio error: %s\n", Pa_GetErrorText(err));
         return EXIT_FAILURE;
     }
+    printf("Portaudio terminated.\n");
     
     return EXIT_SUCCESS;
 }
